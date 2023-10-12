a8db07909e2dbd0f3ec9378229de74652bae013a
sushiyade <68842066+sushiyade@users.noreply.github.com>
2023-10-12 23:51:03 +0800

    Fix Checkstyle

--- src/test/java/seedu/address/model/event/TimeStartTest.java
+++ src/test/java/seedu/address/model/event/TimeStartTest.java
@@ -1,11 +1,11 @@
 package seedu.address.model.event;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.Test;
 
