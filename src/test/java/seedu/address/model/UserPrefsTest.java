b0b6fb5f17cf3340fe60a7c6794a04dffac0ef74
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-10-14 02:53:21 +0800

    Update UserPrefsTest and JsonUserPrefsStorageTest

--- src/test/java/seedu/address/model/UserPrefsTest.java
+++ src/test/java/seedu/address/model/UserPrefsTest.java
@@ -1,5 +1,32 @@
 package seedu.address.model;
 
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 public class UserPrefsTest {
 
+    @Test
+    public void setGuiSettings_nullGuiSettings_throwsNullPointerException() {
+        UserPrefs userPref = new UserPrefs();
+        assertThrows(NullPointerException.class, () -> userPref.setGuiSettings(null));
+    }
+
+    @Test
+    public void setAddressBookFilePath_nullPath_throwsNullPointerException() {
+        UserPrefs userPrefs = new UserPrefs();
+        assertThrows(NullPointerException.class, () -> userPrefs.setAddressBookFilePath(null));
+    }
+
+    @Test
+    public void setEventsFilePath_nullPath_throwsNullPointerException() {
+        UserPrefs userPrefs = new UserPrefs();
+        assertThrows(NullPointerException.class, () -> userPrefs.setEventsFilePath(null));
+    }
+
+    @Test
+    public void setFinanceFilePath_nullPath_throwsNullPointerException() {
+        UserPrefs userPrefs = new UserPrefs();
+        assertThrows(NullPointerException.class, () -> userPrefs.setFinanceFilePath(null));
+    }
 }
