76c84f400428fad9962af4728f1afeee3c95a997
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-10-13 11:12:19 +0800

    Update Tabs Test Cases

--- src/test/java/seedu/address/ui/tab/EventsTabTest.java
+++ src/test/java/seedu/address/ui/tab/EventsTabTest.java
@@ -1,19 +1,4 @@
 package seedu.address.ui.tab;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import org.junit.jupiter.api.Test;
-
 public class EventsTabTest {
-    @Test
-    public void constructor_initialize_exceptionThrown() {
-        ExceptionInInitializerError thrown = assertThrows(
-                ExceptionInInitializerError.class,
-                EventsTab::new,
-                "Expected to throw, but it didn't"
-        );
-
-        assertNull(thrown.getMessage());
-    }
 }
--- src/test/java/seedu/address/ui/tab/FinanceTabTest.java
+++ src/test/java/seedu/address/ui/tab/FinanceTabTest.java
@@ -1,19 +1,5 @@
 package seedu.address.ui.tab;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import org.junit.jupiter.api.Test;
-
 public class FinanceTabTest {
-    @Test
-    public void constructor_initialize_exceptionThrown() {
-        ExceptionInInitializerError thrown = assertThrows(
-                ExceptionInInitializerError.class,
-                FinanceTab::new,
-                "Expected to throw, but it didn't"
-        );
 
-        assertNull(thrown.getMessage());
-    }
 }
