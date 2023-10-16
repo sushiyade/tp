2adc8fff44f56a40a687fa2f239d066e4bde9c98
chew01 <chewjh19@gmail.com>
2023-10-17 00:09:49 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/finance/DeleteFinanceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/finance/DeleteFinanceCommandTest.java
@@ -5,8 +5,6 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
