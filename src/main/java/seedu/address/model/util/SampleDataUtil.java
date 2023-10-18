1edd3ad3bcd585741118b53e09895a08ffda40dc
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-10-18 18:30:59 +0800

    Fix Parsing Issues

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -26,21 +26,21 @@ public class SampleDataUtil {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
-                new Company("Google"), new TelegramName("AlexYeoh")),
+                new Company("Google"), new TelegramName("@AlexYeoh")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                 new Company("FAPro"),
-                new TelegramName("BerniceYu")),
+                new TelegramName("@BerniceYu")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                     new Address(""),
                     new Company("NUS"),
-                    new TelegramName("charOlive")),
+                    new TelegramName("@charOlive")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 69 Lorong Jalan, #3-69"), new Company("Google"), new TelegramName("DavidLi")),
+                new Address("Blk 69 Lorong Jalan, #3-69"), new Company("Google"), new TelegramName("@DavidLi")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 69 Lorong Batok, #5-55"), new Company("Google"), new TelegramName("IrfanIbrahim")),
+                new Address("Blk 69 Lorong Batok, #5-55"), new Company("Google"), new TelegramName("@IrfanIbrahim")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address(""), new Company("Google"), new TelegramName("balakroiii"))
+                new Address(""), new Company("Google"), new TelegramName("@balakroiii"))
         };
     }
 
