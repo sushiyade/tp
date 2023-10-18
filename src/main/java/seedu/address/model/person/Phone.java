434e97e8b030d11a30527a448d66818d630f130f
sushiyade <68842066+sushiyade@users.noreply.github.com>
2023-10-18 18:24:30 +0800

    Fix Test

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -12,8 +12,8 @@ public class Phone {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Phone numbers should only contain numbers, and it should be at least 5 digits long";
-    public static final String VALIDATION_REGEX = "^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?"
-            + "[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$";
+    public static final String VALIDATION_REGEX = "^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?"
+            + "\\d{1,4}[-.\\s]?\\d{1,9}$";
 
     public final String value;
 
