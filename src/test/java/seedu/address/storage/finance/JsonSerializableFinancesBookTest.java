ecdc558d6c63649e37a79d9abaee7134f6d64d8c
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-10-20 23:54:23 +0800

    Add test case for JsonSerializableFinancesBook

--- src/test/java/seedu/address/storage/finance/JsonSerializableFinancesBookTest.java
+++ src/test/java/seedu/address/storage/finance/JsonSerializableFinancesBookTest.java
@@ -1,17 +1,15 @@
 package seedu.address.storage.finance;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.commons.util.JsonUtil;
-import seedu.address.model.EventsBook;
-import seedu.address.model.FinancesBook;
-import seedu.address.storage.events.JsonSerializableEventsBook;
-import seedu.address.testutil.TypicalEvents;
+import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static seedu.address.testutil.Assert.assertThrows;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.util.JsonUtil;
+import seedu.address.model.FinancesBook;
 
 public class JsonSerializableFinancesBookTest {
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableFinancesBookTest");
