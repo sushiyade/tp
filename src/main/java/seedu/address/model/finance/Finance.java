c96d818173a5575b646c73b9c2793e9235986795
chew01 <chewjh19@gmail.com>
2023-10-27 01:47:52 +0800

    Add timedue class

--- src/main/java/seedu/address/model/finance/Finance.java
+++ src/main/java/seedu/address/model/finance/Finance.java
@@ -7,23 +7,30 @@ import seedu.address.model.person.Person;
  */
 public abstract class Finance {
     /**
-     * Get the amount of the commission.
+     * Get the amount of the finance entry.
      *
-     * @return The amount of the commission.
+     * @return The amount of the finance entry.
      */
     public abstract Amount getAmount();
 
     /**
-     * Get the client or payee associated with the commission.
+     * Get the client or payee associated with the finance entry.
      *
-     * @return The client or payee associated with the commission.
+     * @return The client or payee associated with the finance entry.
      */
     public abstract Person getClient();
 
     /**
-     * Get the description of the commission.
+     * Get the description of the finance entry.
      *
-     * @return The description of the commission.
+     * @return The description of the finance entry.
      */
     public abstract Description getDescription();
+
+    /**
+     * Get the time due of the finance entry.
+     *
+     * @return The time due of the finance entry.
+     */
+    public abstract TimeDue getTimeDue();
 }
