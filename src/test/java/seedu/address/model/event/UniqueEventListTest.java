0890952930bad31fee65b5b9e72c5e7b6fa1e72e
sushiyade <68842066+sushiyade@users.noreply.github.com>
2023-11-02 11:52:48 +0800

    Add tests for codecov

---
+++ src/test/java/seedu/address/model/event/UniqueEventListTest.java
@@ -0,0 +1,40 @@
+package seedu.address.model.event;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalEvents.EVENT5;
+
+import org.junit.jupiter.api.Test;
+
+class UniqueEventListTest {
+    private final UniqueEventList uniqueEventList = new UniqueEventList();
+
+    @Test
+    public void contains_nullEvent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.contains(null));
+    }
+
+    @Test
+    public void contains_eventNotInList_returnsFalse() {
+        assertFalse(uniqueEventList.contains(EVENT5));
+    }
+
+    @Test
+    public void contains_eventInList_returnsTrue() {
+        uniqueEventList.add(EVENT5);
+        assertTrue(uniqueEventList.contains(EVENT5));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, ()
+                -> uniqueEventList.asUnmodifiableObservableList().remove(0));
+    }
+
+    @Test
+    public void toStringMethod() {
+        assertEquals(uniqueEventList.asUnmodifiableObservableList().toString(), uniqueEventList.toString());
+    }
+}
