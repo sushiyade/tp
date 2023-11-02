e6a4aa8f119b53956b47f24d0f93c4999cedcc20
sushiyade <68842066+sushiyade@users.noreply.github.com>
2023-11-02 15:23:49 +0800

    Add test for contains

--- src/test/java/seedu/address/model/event/UniqueEventListTest.java
+++ src/test/java/seedu/address/model/event/UniqueEventListTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalEvents.EVENT1;
+import static seedu.address.testutil.TypicalEvents.EVENT2;
+import static seedu.address.testutil.TypicalEvents.EVENT3;
 import static seedu.address.testutil.TypicalEvents.EVENT5;
 
 import org.junit.jupiter.api.Test;
@@ -24,6 +27,24 @@ class UniqueEventListTest {
     @Test
     public void contains_eventInList_returnsTrue() {
         uniqueEventList.add(EVENT5);
+        assertTrue(uniqueEventList.contains(EVENT5));
+    }
+
+    @Test
+    public void contains_eventNotInListWithMultipleEvents_returnsFalse() {
+        uniqueEventList.add(EVENT1);
+        uniqueEventList.add(EVENT2);
+        uniqueEventList.add(EVENT3);
+
+        assertFalse(uniqueEventList.contains(EVENT5));
+    }
+
+    @Test
+    public void contains_eventInListWithMultipleEvents_returnsFalse() {
+        uniqueEventList.add(EVENT1);
+        uniqueEventList.add(EVENT5);
+        uniqueEventList.add(EVENT3);
+
         assertTrue(uniqueEventList.contains(EVENT5));
     }
 
